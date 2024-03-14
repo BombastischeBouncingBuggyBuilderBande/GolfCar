35be297725f876ca60b5c9ea1c07645a133fe9c4
reney <rene.weissteiner@gmail.com>
2024-03-14 12:34:26 +0100

    - Diary Login and Entry-View works

--- Diary/diary_delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-require "Classes/Datenbank.php";
-echo "heyho";
-$eintragID = isset($_POST['IDEintragDeleteText']) ? $_POST['IDEintragDeleteText'] : '20';
-$db = new Datenbank();
-$db->deleteEintrag("20");
-echo (int)$eintragID;
-?>
