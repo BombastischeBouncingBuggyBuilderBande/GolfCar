530d960acf2e31437b081f1e6861c600bfc8b600
reney <rene.weissteiner@gmail.com>
2024-03-20 19:29:31 +0100

    - Improved "Bauteile" UI

--- Bauteile/bauteile.php
+++ Bauteile/bauteile.php
@@ -62,24 +62,7 @@
                 <a href="link/to/purchase-location" target="_blank">Kaufen</a>
             </div>
         </div>
-        <div class="product-card">
-            <img src="resources/resources_bauteile/exampleImg2.png" alt="Produktname">
-            <div class="product-info">
-                <h2>Produktname</h2>
-                <p class="price">99,99 €</p>
-                <p class="description">Eine kurze Beschreibung des Produkts, wo der Nutzen beschrieben wird.</p>
-                <a href="link/to/purchase-location" target="_blank">Kaufen</a>
-            </div>
-        </div>
-        <div class="product-card">
-            <img src="resources/resources_bauteile/exampleImg2.png" alt="Produktname">
-            <div class="product-info">
-                <h2>Produktname</h2>
-                <p class="price">99,99 €</p>
-                <p class="description">Eine kurze Beschreibung des Produkts, wo der Nutzen beschrieben wird.</p>
-                <a href="link/to/purchase-location" target="_blank">Kaufen</a>
-            </div>
-        </div>
+
     </div>
     <script src="Bauteile/script_bauteile.js"></script>
 </body>
