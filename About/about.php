eb553ae358f92189e5ba18d749c478e42df40536
reney <rene.weissteiner@gmail.com>
2024-03-27 09:15:30 +0100

    - updated control and mainpage

--- About/about.php
+++ About/about.php
@@ -9,19 +9,19 @@
     </div>
     <div class="about_container">
         <h1 class="About_name">Pillip</h1>
-        <p class="rolle">Bauleiter</p>
+        <p class="rolle">Golfcar</p>
         <img src="resources/aboutPlaceholder.png" alt="Person">
 
     </div>
     <div class="about_container">
         <h1 class="About_name">Alex</h1>
-        <p class="rolle">LeiterLeiter</p>
+        <p class="rolle">Planung</p>
         <img src="resources/aboutPlaceholder.png" alt="Person">
 
     </div>
     <div class="about_container">
         <h1 class="About_name">René</h1>
-        <p class="rolle">Website Leiter</p>
+        <p class="rolle">Website</p>
         <img src="resources/aboutPlaceholder.png" alt="Person">
     </div>
 
