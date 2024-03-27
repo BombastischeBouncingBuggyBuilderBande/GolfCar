948b2156e27c9195c294429ec8016e2f2bc8a4e0
reney <rene.weissteiner@gmail.com>
2024-03-27 11:35:08 +0100

    - Bauteile responsiveness

--- Bauteile/bauteile.php
+++ Bauteile/bauteile.php
@@ -7,12 +7,12 @@
     <link rel="stylesheet" href="Bauteile/style_bauteile.css">
 </head>
 <body>
-<div class="container">
-    <button class="buttons" id="prevBtn">◄</button>
-    <div class="product-scroll-container" id="product-scroll-container">
+<div class="bauteile-container">
+    <button class="bauteile-buttons" id="bauteile-prevBtn">◄</button>
+    <div class="bauteile-product-scroll-container" id="bauteile-product-scroll-container">
         <!-- Product cards will be dynamically added here -->
     </div>
-    <button class="buttons" id="nextBtn">►</button>
+    <button class="bauteile-buttons" id="bauteile-nextBtn">►</button>
 </div>
 <script src="Bauteile/script_bauteile.js"></script>
 </body>
