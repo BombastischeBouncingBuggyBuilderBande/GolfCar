2b0216c8503cd0c22af6d4502c4480387b16f08f
reney <rene.weissteiner@gmail.com>
2024-03-27 11:51:23 +0100

    - Bauteile responsiveness

--- Bauteile/script_bauteile.js
+++ Bauteile/script_bauteile.js
@@ -107,10 +107,10 @@ document.addEventListener('DOMContentLoaded', () => {
             productCard.innerHTML = `
                 <img id="bauteile-product-img" src="${product.img}" alt="${product.name}">
                 <div class="bauteile-product-info">
-                    <h2>${product.name}</h2>
+                    <h2 class="bauteile-product-header">${product.name}</h2>
                     <p class="bauteile-price">${product.price}</p>
                     <p class="bauteile-description">${product.description}</p>
-                    <a href="${product.link}" target="_blank">Kaufen</a>
+                    <a class="bauteile-product-link" href="${product.link}" target="_blank">Kaufen</a>
                 </div>
             `;
             productCard.addEventListener('click', () => {
