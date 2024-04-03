8bf366d77f116997b095b0ace385e64cec28350a
reney <rene.weissteiner@gmail.com>
2024-04-03 15:19:47 +0200

    - Datenbank Websitestates initialized - Contact in main menu background text fixed

--- Teamspace/Datenbank_WebsiteStates.php
+++ Teamspace/Datenbank_WebsiteStates.php
@@ -1,1 +1,33 @@
- 
+<?php
+/**
+ * Datenbankverwaltung für den Status der Website.
+ *
+ * @author René
+ */
+class Datenbank extends \PDO
+{
+    private $host = 'localhost';
+    private $db = 'Tagebuch';
+    private $user = 'root';
+    private $pass = '';
+    private $charset = 'utf8mb4';
+    private $pdo;
+
+    /**
+     * Konstruktor, stellt eine Verbindung zur Datenbank her.
+     */
+    public function __construct()
+    {
+        $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false,
+        ];
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (\PDOException $e) {
+            throw new \PDOException($e->getMessage(), (int)$e->getCode());
+        }
+    }
+}
