2fd1e187dcbd61bd3768cc1a659a681990c7e3cf
solotov-val <prillerpatrick@gmail.com>
2024-04-10 22:03:10 +0200

    removed local path

--- Teamspace/write_to_json.php
+++ Teamspace/write_to_json.php
@@ -1,7 +1,11 @@
 <?php
 $jsonData = file_get_contents('php://input');
 
-$filePath = 'C:\xampp\htdocs\Golfcar\Teamspace\state.json';
+// Use __DIR__ to get the directory of the current script.
+// Then construct the path to the 'state.json' file relative to this script.
+// This makes your script more flexible and portable.
+$filePath = __DIR__ . '/state.json';
+
 $writeResult = file_put_contents($filePath, $jsonData);
 
 if ($writeResult !== false) {
@@ -11,8 +15,3 @@ if ($writeResult !== false) {
     http_response_code(500);
     echo 'Failed to update JSON file. Error writing to file.';
 }
-
-
-
-
-
