2999bc3b35d8186913cc934b203ef29deb9a2584
solotov-val <prillerpatrick@gmail.com>
2024-04-11 10:04:26 +0200

    credentials

--- Teamspace/config.php
+++ Teamspace/config.php
@@ -1,7 +1,7 @@
 <?php
 // Define database credentials
-$db_host = '5.231.1.40';
+$db_host = 'localhost'; //'5.231.1.40';
 $db_name = 'Tagebuch';
 $db_user = 'root';
 $db_port = '4444';
-$db_pass = 'ipfm6wtdxrb3zqav';
+$db_pass = 'AltaBadia420!';
