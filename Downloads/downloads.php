f070c1e7586c9f62f5ca7f3a348c0e33cb001e38
Alex Pedevilla <stpedale@bx.fallmerayer.it>
2024-04-12 09:57:56 +0200

    Downloads right files

--- Downloads/downloads.php
+++ Downloads/downloads.php
@@ -9,41 +9,43 @@
 <body>
     <div class="download_container">
         <h1>Downloads</h1>
-        <ul class="downloads-list">
-            <?php
-            // Directory where your files are stored
-            $directory = 'Downloads/files/';
+        <div class="links_container">
+            <ul class="downloads-list">
+                <?php
+                // Directory where your files are stored
+                $directory = 'Downloads/files/';
 
-            // File type icons mapping
-            $fileIcons = array(
-                'pdf' => 'pdf_icon.png',
-                'doc' => 'doc_icon.png',
-                'docx' => 'doc_icon.png',
-                'jpg' => 'jpg_icon.png',
-                // Add more file types and their respective icons here
-                'default' => 'default_icon.png' // Default icon for unknown file types
-            );
+                // File type icons mapping
+                $fileIcons = array(
+                    'pdf' => 'pdf_icon.png',
+                    'doc' => 'doc_icon.png',
+                    'docx' => 'doc_icon.png',
+                    'jpg' => 'jpg_icon.png',
+                    // Add more file types and their respective icons here
+                    'default' => 'default_icon.png' // Default icon for unknown file types
+                );
 
-            // Open the directory
-            if ($handle = opendir($directory)) {
-                // Loop through the directory
-                while (false !== ($entry = readdir($handle))) {
-                    // Exclude current directory and parent directory
-                    if ($entry != "." && $entry != "..") {
-                        // Get file extension
-                        $extension = strtolower(pathinfo($entry, PATHINFO_EXTENSION));
+                // Open the directory
+                if ($handle = opendir($directory)) {
+                    // Loop through the directory
+                    while (false !== ($entry = readdir($handle))) {
+                        // Exclude current directory and parent directory
+                        if ($entry != "." && $entry != "..") {
+                            // Get file extension
+                            $extension = strtolower(pathinfo($entry, PATHINFO_EXTENSION));
 
-                        // Check if the file extension exists in the fileIcons array
-                        $icon = isset($fileIcons[$extension]) ? $fileIcons[$extension] : $fileIcons['default'];
+                            // Check if the file extension exists in the fileIcons array
+                            $icon = isset($fileIcons[$extension]) ? $fileIcons[$extension] : $fileIcons['default'];
 
-                        // Display download link for each file
-                        echo '<li><a class="download-link" href="' . $directory . $entry . '" download><img class="download-icon" src="Downloads/icons/' . $icon . '" alt="Download">' . $entry . '</a></li>';
+                            // Display download link for each file
+                            echo '<li><a class="download-link" href="' . $directory . $entry . '" download><img class="download-icon" src="Downloads/icons/' . $icon . '" alt="Download">' . $entry . '</a></li>';
+                        }
                     }
+                    // Close directory handle
+                    closedir($handle);
                 }
-                // Close directory handle
-                closedir($handle);
-            }
-            ?>
-        </ul>
+                ?>
+            </ul>
+        </div>
     </div>
 </body>
